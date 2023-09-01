82b2b4e1aeaac39aa1d544ee5f7e94ea5e9c2b27
Pablo <pablito@me.com>
2023-09-01 19:20:38 +0100

    Created API calls to generate descriptions, tags and images

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+const readline = require('readline')
+const { generateMeta, generateImage } = require('./controllers/openaiController')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question("YouTube Video Title: \n", (title) => generateMeta(title))
+// rl.question("Thumbnail description: \n", (desc) => generateImage(desc))
